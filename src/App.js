92c37549ee8014dc1236bdea5380cd7de7a10b2d
Viktoriia Hilenko <80678378+vixxxyg@users.noreply.github.com>
2024-11-18 22:34:39 +0100

    navbar-component

--- src/App.js
+++ src/App.js
@@ -11,31 +11,6 @@ import linkedIn from "./images/icons/linkedIn.svg";
 function App() {
   return (
           <div className="App">
-             <nav className="nav">
-              <div className="container">
-                  <div className="nav-row">
-                    <a href="./index.html" className="logo"><strong>Freelancer</strong> Portfolio</a>
-                      
-                    <button className="dark-mode-btn">
-                        <img src={sun} alt="Light mode" className="dark-mode-btn__icon" />
-                        <img src={moon} alt="Dark mode" className="dark-mode-btn__icon" />
-                    </button>
-                      
-                    <ul className="nav-list">
-                        <li className="nav-list__item">
-                            <a href="./index.html" className="nav-list__link nav-list__link--active">Projects</a>
-                        </li>
-                        <li className="nav-list__item">
-                            <a href="./skills.html" className="nav-list__link">Skills</a>
-                        </li>
-                        <li className="nav-list__item">
-                            <a href="./contacts.html" className="nav-list__link">Contacts</a>
-                        </li>
-                    </ul>
-                  </div>
-              </div>
-             </nav>
-
               <header className="header">
                   <div className="header__wrapper">
                       <h1 className="header__title">
