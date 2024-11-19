b40228b7962c0498a30531a23b2afb6ae0d81aa1
Viktoriia Hilenko <80678378+vixxxyg@users.noreply.github.com>
2024-11-19 21:20:05 +0100

    bug-fix-3

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,11 @@
 import Header from "./../components/header/Header.js";
-import Main from "./../components/main/Main.js";
+import Projects from "./../components/main/Main.js";
 
 function Home () {
     return ( 
         <>
             <Header />
-            <Main />
+            <Projects />
         </>
      );
 }
