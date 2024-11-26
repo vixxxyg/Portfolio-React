87bf2ba014d2be21dda99399da9276d7696e6928
Viktoriia Hilenko <80678378+vixxxyg@users.noreply.github.com>
2024-11-26 22:38:41 +0100

    adds correct route to each project

--- src/components/project/Project.js
+++ src/components/project/Project.js
@@ -1,14 +1,15 @@
- import "./styles.css";
+import {NavLink} from "react-router-dom";
+import "./styles.css";
  
- function Project ({title, img}) {
+ const Project = ({title, img, index}) => {
     return ( 
+        <NavLink to={`/project/${index}`}>
         <li className="project">
-            <a href="./project">
-                <img src={img} alt="{title}" className="project__img" /> 
+                <img src={img} alt={title} className="project__img" /> 
                 <h3 className="project__title">{title}</h3>
-            </a> 
         </li>
+        </NavLink>
      );
- }
+}
   
  export default Project;
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -14,6 +14,7 @@ const Projects = () => {
                             key={index} 
                             title={project.title} 
                             img={project.img} 
+                            index={index}
                         />
                     );
                 })}
