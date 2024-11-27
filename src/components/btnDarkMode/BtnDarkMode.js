ff44914765b1f0b20fd127a987e66d80d9111ccf
Viktoriia Hilenko <80678378+vixxxyg@users.noreply.github.com>
2024-11-27 20:49:50 +0100

    corrects mode switching

--- src/components/btnDarkMode/BtnDarkMode.js
+++ src/components/btnDarkMode/BtnDarkMode.js
@@ -1,15 +1,29 @@
+import {useState, useEffect, useRef} from 'react';
 import "./styles.css";
 import sun from "./sun.svg";
 import moon from "./moon.svg";
+import React from "react";
 
 const BtnDarkMode = () => {
+    const [darkMode, setDarkMode] = useState('light');
+    const btnRef = useRef(null);
+
+    useEffect(() => {
+        if (darkMode === 'dark') {
+            document.body.classList.add('dark')
+        } else {
+            document.body.classList.remove('dark')
+        }
+    }, [darkMode]);
    
     const toggleDarkMode = () => {
-        console.log('toggleDarkMode Fired!');
+        setDarkMode((currentValue) => {
+            return currentValue === 'light' ? 'dark' : 'light';
+        })
     }
     
     return ( 
-        <button className="dark-mode-btn" onClick={toggleDarkMode}>
+        <button ref={btnRef} className="dark-mode-btn" onClick={toggleDarkMode}>
             <img src={sun} alt="Light mode" className="dark-mode-btn__icon" />
             <img src={moon} alt="Dark mode" className="dark-mode-btn__icon" />
         </button>
